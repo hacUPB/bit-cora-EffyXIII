8484490cc18cbe9827b2ce6b733bdf18f43846f6
EffyXIII <145070591+EffyXIII@users.noreply.github.com>
2025-07-31 09:27:26 -0500

    Update Reto1.asm

--- Actividad2/Reto/Reto1.asm
+++ Actividad2/Reto/Reto1.asm
@@ -22,3 +22,9 @@ D=A
 @punt
 A=M
 M=D
+
+
+//6. Ahora vas a usar un puntero para leer la posición de memoria a la que este apunta
+//es decir, vas a leer por medio del puntero la variable cuya dirección está almacenada en él.
+@punt 
+D=M
